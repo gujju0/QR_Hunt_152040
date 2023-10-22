f3a4a3711ce5b63dac2a79f95e30016be31d6f0d
gujju0 <20ce03vatsal@gmail.com>
2023-10-22 14:27:13 +0530

    init

---
+++ js/questions.js
@@ -0,0 +1,81 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Both client and server release _________ connection after a page has been transferred",
+    answer: "Pointer",
+    options: [
+      "AV102-C",
+      "AV110-B",
+      "AV003-A",
+      "AV006-A"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which motor is NOT suitable for use as a DC machine?",
+    answer: "Blockchain",
+    options: [
+      "AV104-D",
+      "AV006-B",
+      "AV102-D",
+      "AV112-C"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Most modern TV's draw power even if turned off. The circuit the power is used in does what function?",
+    answer: "Git hub",
+    options: [
+      "AV007-B",
+      "AV107-C",
+      "AV110-A",
+      "AV114-D"
+    ]
+  },
+  {
+    numb: 4,
+    question: "How is stack found ?",
+    answer: "Research",
+    options: [
+      "AV002-B",
+      "AV114-C",
+      "AV102-D",
+      "AV012-A"
+    ]
+  },
+  {
+    numb: 5,
+    question: "What does the mechanism followed by stack",
+    answer: "Khaman",
+    options: [
+      "AV103-A",
+      "AV108-A",
+      "AV012-B",
+      "AV102-C"
+    ]
+  },
+ 
+  {
+    numb: 6,
+    question: "In which decade was the American Institute of Electrical Engineers (AIEE) founded?",
+    answer: "Gulab Jamun",
+    options: [
+      "AV103-C",
+      "AV101-B",
+      "AV003-C",
+      "AV111-A"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What frequency range is the High Frequency band?",
+    answer: "Tools",
+    options: [
+      "AV003-c",
+      "AV101-A",
+      "AV011-A",
+      "AV004-B"
+    ]
+  },
+];
